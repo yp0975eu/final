cc015c5ea44cd084adc0a48dd1763b32af0970dc
brndn adms <hey@brandenadams.com>
2020-05-14 15:06:04 -0500

    route static assets

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const entriesApi = require('./routes/entries')
-
+const path = require('path')
 
 const app = express()
+app.use(express.static(__dirname, 'client', 'dist'))
+
 app.use(bodyParser.json())
 app.use('/api', entriesApi)
 
